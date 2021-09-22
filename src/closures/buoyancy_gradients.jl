46afad2e933484306c25f1ac7e5b6ee10e40a0a7
yairchn <yairchn@caltech.edu>
2021-09-22 13:06:38 -0700

    compute exner function from thermodynamic state
    use full phase partitioning

--- src/closures/buoyancy_gradients.jl
+++ src/closures/buoyancy_gradients.jl
@@ -5,15 +5,18 @@ function buoyancy_gradients(param_set, bg_model::Tan2018)
     R_d = CPP.R_d(param_set)
     R_v = CPP.R_v(param_set)
 
-    # buoyancy_gradients
-    phase_part = TD.PhasePartition(0.0, 0.0, 0.0) # assuming R_d = R_m
+    # TODO: when an anelastic thermodynamic is present we should use ts_dry for exner
+    phase_part = TD.PhasePartition(bg_model.qt_dry, 0.0, 0.0)
     Π = TD.exner_given_pressure(param_set, bg_model.p0, phase_part)
-
     prefactor = g * (R_d / bg_model.alpha0 / bg_model.p0) * Π
     ∂b∂θl_dry = prefactor * (1 + (molmass_ratio - 1) * bg_model.qt_dry)
     ∂b∂qt_dry = prefactor * bg_model.th_dry * (molmass_ratio - 1)
 
     if bg_model.en_cld_frac > 0.0
+        ql_cloudy = bg_model.qt_cloudy - bg_model.qv_cloudy
+        # TODO: when an anelastic thermodynamic is present we should use ts_cloudy for exner
+        phase_part = TD.PhasePartition(bg_model.qt_cloudy, ql_cloudy, 0.0)
+        Π = TD.exner_given_pressure(param_set, bg_model.p0, phase_part)
         ts_cloudy = TD.PhaseEquil_pθq(param_set, bg_model.p0, bg_model.th_cloudy, bg_model.qt_cloudy)
         lh = TD.latent_heat_vapor(param_set, bg_model.t_cloudy)
         cp_m = TD.cp_m(ts_cloudy)
