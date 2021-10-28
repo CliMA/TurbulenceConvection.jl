852d829dbd6c06c5135879f558d31dd2476e6a68
ilopezgp <ilopezgp@gmail.com>
2021-10-28 15:20:24 -0700

    Add specific entropy and flux diagnostic.

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -50,6 +50,7 @@ function io_dictionary_aux(state)
         "buoyancy_mean" => (; dims = ("zc", "t"), group = "profiles", field = center_aux_grid_mean(state).buoy),
         "temperature_mean" => (; dims = ("zc", "t"), group = "profiles", field = center_aux_grid_mean(state).T),
         "RH_mean" => (; dims = ("zc", "t"), group = "profiles", field = center_aux_grid_mean(state).RH),
+        "s_mean" => (; dims = ("zc", "t"), group = "profiles", field = center_aux_grid_mean(state).s),
         "ql_mean" => (; dims = ("zc", "t"), group = "profiles", field = center_aux_grid_mean(state).q_liq),
         "qi_mean" => (; dims = ("zc", "t"), group = "profiles", field = center_aux_grid_mean(state).q_ice),
         "tke_mean" => (; dims = ("zc", "t"), group = "profiles", field = center_aux_grid_mean(state).tke),
@@ -106,6 +107,9 @@ function io_dictionary_aux(state)
         "env_RH" => (; dims = ("zc", "t"), group = "profiles", field = center_aux_environment(state).RH),
         "env_thetal" => (; dims = ("zc", "t"), group = "profiles", field = center_aux_environment(state).θ_liq_ice),
         "env_cloud_fraction" => (; dims = ("zc", "t"), group = "profiles", field = center_aux_environment(state).cloud_fraction),
+        "massflux_s" => (; dims = ("zf", "t"), group = "profiles", field = face_aux_grid_mean(state).massflux_s),
+        "diffusive_flux_s" => (; dims = ("zf", "t"), group = "profiles", field = face_aux_grid_mean(state).diffusive_flux_s),
+        "total_flux_s" => (; dims = ("zf", "t"), group = "profiles", field = face_aux_grid_mean(state).massflux_s .+ face_aux_grid_mean(state).diffusive_flux_s),
 
         "qr_mean" => (; dims = ("zc", "t"), group = "profiles", field = center_prog_rain(state).qr),
 
@@ -142,10 +146,18 @@ function compute_diagnostics!(edmf, gm, grid, state, Case, TS)
     gm.lwp = 0.0
     gm.iwp = 0.0
     ρ0_c = center_ref_state(state).ρ0
+    p0_c = center_ref_state(state).p0
     aux_gm = center_aux_grid_mean(state)
+    aux_en = center_aux_environment(state)
+    aux_up = center_aux_updrafts(state)
+    aux_tc_f = face_aux_tc(state)
+    aux_gm_f = face_aux_grid_mean(state)
     kc_toa = kc_top_of_atmos(grid)
     gm.cloud_base = grid.zc[kc_toa]
     gm.cloud_top = 0.0
+    param_set = parameter_set(gm)
+    prog_gm = center_prog_grid_mean(state)
+    n_updrafts = edmf.UpdVar.n_updrafts
 
     @inbounds for k in real_center_indices(grid)
         gm.lwp += ρ0_c[k] * aux_gm.q_liq[k] * grid.Δz
@@ -153,6 +165,28 @@ function compute_diagnostics!(edmf, gm, grid, state, Case, TS)
         if TD.has_condensate(aux_gm.q_liq[k] + aux_gm.q_ice[k])
             gm.cloud_base = min(gm.cloud_base, grid.zc[k])
             gm.cloud_top = max(gm.cloud_top, grid.zc[k])
+        end
+        ts = thermo_state_pθq(param_set, p0_c[k], prog_gm.θ_liq_ice[k], prog_gm.q_tot[k])
+        aux_gm.s[k] = TD.specific_entropy(ts)
+        ts_en = thermo_state_pθq(param_set, p0_c[k], aux_en.θ_liq_ice[k], aux_en.q_tot[k])
+        aux_en.s[k] = TD.specific_entropy(ts_en)
+        @inbounds for i in 1:n_updrafts
+            if aux_up[i].area[k] > 0.0
+                ts_up = thermo_state_pθq(param_set, p0_c[k], aux_up[i].θ_liq_ice[k], aux_up[i].q_tot[k])
+                aux_up[i].s[k] = TD.specific_entropy(ts_up)
+            end
+        end
+    end
+    m_bcs = (; bottom = SetValue(0), top = SetValue(0))
+    @inbounds for k in real_face_indices(grid)
+        s_dual = dual_centers(aux_en.s, grid, k)
+        # TODO(ilopezgp): Fix bottom gradient
+        ∇s_f = ∇c2f(s_dual, grid, k; bottom = SetGradient(0), top = SetGradient(0))
+        aux_gm_f.diffusive_flux_s[k] = -aux_tc_f.ρ_ae_KH[k] * ∇s_f
+        s_en_f = interpc2f(aux_en.s, grid, k; m_bcs...)
+        @inbounds for i in 1:n_updrafts
+            s_up_f = interpc2f(aux_up[i].s, grid, k; m_bcs...)
+            aux_gm_f.massflux_s[k] += edmf.m[i, k] * (s_up_f - s_en_f)
         end
     end
     return
