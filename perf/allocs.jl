9e9d76ceab7d1fe4dabd0aa34d96333fa255fd46
Charles Kawczynski <kawczynski.charles@gmail.com>
2021-11-19 17:10:47 -0800

    Try printing full list of allocations
    Try plotting allocs, filt compile tc in full list

--- perf/allocs.jl
+++ perf/allocs.jl
@@ -43,6 +43,22 @@ end
 
 function plot_allocs(case_name, allocs_per_case, n_unique_bytes)
     p = Plots.plot()
+    @info "Allocations for $case_name"
+
+    filename_only(fn) = first(split(fn, ".jl")) * ".jl"
+    function compile_tc(fn, linenumber)
+        c1 = endswith(filename_only(fn), "TurbulenceConvection.jl")
+        c2 = linenumber == 1
+        return c1 && c2
+    end
+
+    filter!(x -> x.bytes ≠ 0, allocs_per_case)
+    filter!(x -> !compile_tc(x.filename, x.linenumber), allocs_per_case)
+
+    for alloc in allocs_per_case
+        println(alloc)
+    end
+    println("Number of allocating sites: $(length(allocs_per_case))")
     case_bytes = getproperty.(allocs_per_case, :bytes)[end:-1:1]
     case_filename = getproperty.(allocs_per_case, :filename)[end:-1:1]
     case_linenumber = getproperty.(allocs_per_case, :linenumber)[end:-1:1]
@@ -51,11 +67,8 @@ function plot_allocs(case_name, allocs_per_case, n_unique_bytes)
     linenumbers = Int[]
     loc_ids = String[]
     for (bytes, filename, linenumber) in zip(case_bytes, case_filename, case_linenumber)
-        filename_only = first(split(filename, ".jl")) * ".jl"
-        if endswith(filename_only, "TurbulenceConvection.jl") && linenumber == 1
-            continue # Skip loading module
-        end
-        loc_id = "$filename_only" * "$linenumber"
+        compile_tc(filename, linenumber) && continue # Skip loading module
+        loc_id = "$(filename_only(filename))" * "$linenumber"
         if !(bytes in all_bytes) && !(loc_id in loc_ids)
             push!(all_bytes, bytes)
             push!(filenames, filename)
@@ -73,18 +86,24 @@ function plot_allocs(case_name, allocs_per_case, n_unique_bytes)
     xtick_name(filename, linenumber) = "$filename, line number: $linenumber"
     markershape = (:square, :hexagon, :circle, :star, :utriangle, :dtriangle)
     for (bytes, filename, linenumber) in zip(all_bytes, filenames, linenumbers)
-        filename_only = first(split(filename, ".jl")) * ".jl"
         Plots.plot!(
             [0],
             [bytes];
             seriestype = :scatter,
-            label = xtick_name(filename_only, linenumber),
+            label = xtick_name(filename_only(filename), linenumber),
             markershape = markershape[1],
             markersize = 1 + bytes / max_bytes * 10,
         )
         markershape = (markershape[end], markershape[1:(end - 1)]...)
     end
-    Plots.plot!(ylabel = "Allocations (KB)", title = case_name)
+    p1 = Plots.plot!(ylabel = "Allocations (KB)", title = case_name)
+    p2 = Plots.plot(
+        1:length(allocs_per_case),
+        getproperty.(allocs_per_case, :bytes)[end:-1:1] ./ 1000;
+        xlabel = "i-th allocating line (sorted)",
+        ylabel = "Allocations (KB)",
+    )
+    Plots.plot(p1, p2, layout = Plots.grid(2, 1))
     Plots.savefig(joinpath(folder, "allocations_$case_name.png"))
 end
 
