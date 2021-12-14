5b2a0781359f940b2e68ebd83873ac1a127772a5
Anna Jaruga <ajaruga@caltech.edu>
2021-12-14 10:57:30 -0800

    update to new parameter name and remove 0.2 CLIMAParameters from compat
    Fix test env
    
    Fix qualified types

--- driver/generate_namelist.jl
+++ driver/generate_namelist.jl
@@ -286,7 +286,7 @@ function Rico(namelist_defaults)
     namelist["time_stepping"]["dt_min"] = 1.5
 
     namelist["microphysics"]["precipitation_model"] = "clima_1m"
-    namelist["microphysics"]["τ_acnv"] = 2500.0
+    namelist["microphysics"]["τ_acnv_rai"] = 2500.0
 
     namelist["meta"]["simname"] = "Rico"
     namelist["meta"]["casename"] = "Rico"
