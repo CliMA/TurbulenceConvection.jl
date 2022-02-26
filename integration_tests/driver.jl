a74a32407fd8bf2034dcac92c51ce5bee50d4ab7
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-02-26 12:14:23 -0800

    Unify driver files

--- integration_tests/driver.jl
+++ integration_tests/driver.jl
@@ -12,28 +12,39 @@ include(joinpath(tc_dir, "post_processing", "compute_mse.jl"))
 include(joinpath(tc_dir, "post_processing", "mse_tables.jl"))
 import .NameList
 
-best_mse = all_best_mse["Nieuwstadt"]
+import ArgParse
 
-case_name = "Nieuwstadt"
+s = ArgParse.ArgParseSettings()
+ArgParse.@add_arg_table s begin
+    "--case"
+    help = "Case to run"
+    arg_type = String
+    default = "Bomex"
+end
+parsed_args = ArgParse.parse_args(ARGS, s)
+case_name = parsed_args["case"]
+
+# if abspath(PROGRAM_FILE) == @__FILE__
+#     default_namelist(nothing)
+# end
+
+best_mse = all_best_mse[case_name]
+
 println("Running $case_name...")
 namelist = NameList.default_namelist(case_name)
 namelist["meta"]["uuid"] = "01"
 ds_tc_filename, return_code = main(namelist)
 
-computed_mse = compute_mse_wrapper(
-    case_name,
-    best_mse,
-    ds_tc_filename;
-    plot_comparison = true,
-    t_start = 6 * 3600,
-    t_stop = 8 * 3600,
-)
+# Post-processing case kwargs
+include(joinpath(tc_dir, "post_processing", "case_kwargs.jl"))
+
+computed_mse = compute_mse_wrapper(case_name, best_mse, ds_tc_filename; case_kwargs[case_name]...)
 
 open("computed_mse_$case_name.json", "w") do io
     JSON.print(io, computed_mse)
 end
 
-@testset "Nieuwstadt" begin
+@testset "$case_name" begin
     for k in keys(best_mse)
         test_mse(computed_mse, best_mse, k)
     end
---
+++ post_processing/case_kwargs.jl
@@ -0,0 +1,69 @@
+case_kwargs = OrderedCollections.OrderedDict()
+
+#! format: off
+case_kwargs["Bomex"] = (;
+    plot_comparison = true,
+    t_start = 4 * 3600,
+    t_stop = 6 * 3600,
+)
+
+case_kwargs["life_cycle_Tan2018"] = (;
+    plot_comparison = true,
+    t_start = 4 * 3600,
+    t_stop = 6 * 3600)
+
+case_kwargs["Soares"] = (;
+    plot_comparison = true,
+    t_start = 6 * 3600,
+    t_stop = 8 * 3600,)
+
+case_kwargs["Rico"] = (;
+    plot_comparison = true,
+    t_start = 22 * 3600,
+    t_stop = 24 * 3600)
+
+case_kwargs["Nieuwstadt"] = (;
+    plot_comparison = true,
+    t_start = 6 * 3600,
+    t_stop = 8 * 3600)
+
+case_kwargs["TRMM_LBA"] = (;
+    plot_comparison = true,
+    t_start = 4 * 3600,
+    t_stop = 6 * 3600)
+
+case_kwargs["ARM_SGP"] = (;
+    plot_comparison = true,
+    t_start = 8 * 3600,
+    t_stop = 11 * 3600)
+
+case_kwargs["GATE_III"] = (;)
+
+case_kwargs["DYCOMS_RF01"] = (;
+    plot_comparison = true,
+    t_start = 2 * 3600,
+    t_stop = 4 * 3600)
+
+case_kwargs["DYCOMS_RF02"] = (;
+    plot_comparison = true,
+    t_start = 4 * 3600,
+    t_stop = 6 * 3600)
+
+case_kwargs["GABLS"] = (;
+    plot_comparison = true,
+    t_start = 7 * 3600,
+    t_stop = 9 * 3600)
+
+case_kwargs["DryBubble"] = (;
+    plot_comparison = true,
+    t_start = 900,
+    t_stop = 1000)
+
+case_kwargs["LES_driven_SCM"] = (;
+    ds_les_filename = joinpath(NameList.les_driven_scm_data_folder(), "Stats.cfsite23_HadGEM2-A_amip_2004-2008.07.nc"),
+    plot_comparison = true,
+    t_start = 3 * 3600,
+    t_stop = 6 * 3600,
+)
+
+#! format: on
