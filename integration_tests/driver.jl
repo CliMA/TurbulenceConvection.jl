5db7d220ed2475a9b711fb8ea9253f84747d1a0f
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-03-05 22:21:18 -0800

    Add more cli options

--- integration_tests/driver.jl
+++ integration_tests/driver.jl
@@ -8,23 +8,28 @@ if run_from_command_line
         help = "Case to run"
         default = "Bomex"
         # Command-line args to change default namelist params
-        "--micro"          # Try other microphysics quadrature
-        "--entr"           # Try other entr-detr models
-        "--stoch_entr"     # Choose type of stochastic entr-detr model
-        "--calibrate_io"   # Test that calibration IO passes regression tests
-        "--skip_io"        # Test that skipping IO passes regression tests
+        "--micro"                  # Try other microphysics quadrature
+        "--entr"                   # Try other entr-detr models
+        "--stoch_entr"             # Choose type of stochastic entr-detr model
+        "--area_limiter_power"     # Choose area limiter power
+        "--entr_dim_scale"         # Choose entrainment dim scale
+        "--calibrate_io"           # Test that calibration IO passes regression tests
+        "--skip_io"                # Test that skipping IO passes regression tests
     end
     parsed_args = ArgParse.parse_args(ARGS, s)
     case_name = parsed_args["case"]
 end
 
 #! format: off
-micro        = run_from_command_line ? parsed_args["micro"] : nothing
-entr         = run_from_command_line ? parsed_args["entr"] : nothing
-stoch_entr   = run_from_command_line ? parsed_args["stoch_entr"] : nothing
-calibrate_io = run_from_command_line ? parsed_args["calibrate_io"] : nothing
-skip_io      = run_from_command_line ? parsed_args["skip_io"] : nothing
+micro              = run_from_command_line ? parsed_args["micro"] : nothing
+entr               = run_from_command_line ? parsed_args["entr"] : nothing
+stoch_entr         = run_from_command_line ? parsed_args["stoch_entr"] : nothing
+calibrate_io       = run_from_command_line ? parsed_args["calibrate_io"] : nothing
+skip_io            = run_from_command_line ? parsed_args["skip_io"] : nothing
+area_limiter_power = run_from_command_line ? parsed_args["area_limiter_power"] : nothing
+entr_dim_scale     = run_from_command_line ? parsed_args["entr_dim_scale"] : nothing
 
+area_limiter_power ≠ nothing && (area_limiter_power = parse(Float64, area_limiter_power))
 calibrate_io ≠ nothing && (calibrate_io = parse(Bool, calibrate_io))
 skip_io ≠ nothing      && (skip_io = parse(Bool, skip_io))
 #! format: on
@@ -48,21 +53,26 @@ import .NameList
 namelist = NameList.default_namelist(case_name)
 
 uuid_suffix(s::String) = "_" * s
+uuid_suffix(s::Number) = "_" * "$s"
 uuid_suffix(::Nothing) = ""
 suffix = uuid_suffix(micro)
 suffix *= uuid_suffix(entr)
 suffix *= uuid_suffix(stoch_entr)
+suffix *= uuid_suffix(area_limiter_power)
+suffix *= uuid_suffix(entr_dim_scale)
 calibrate_io ≠ nothing && (suffix *= "_calibrate_io_$calibrate_io")
 skip_io ≠ nothing && (suffix *= "_skip_io_$skip_io")
 
 namelist["meta"]["uuid"] = "01$suffix"
 
 #! format: off
-micro        ≠ nothing && (namelist["thermodynamics"]["quadrature_type"] = micro)
-entr         ≠ nothing && (namelist["turbulence"]["EDMF_PrognosticTKE"]["entrainment"] = entr)
-stoch_entr   ≠ nothing && (namelist["turbulence"]["EDMF_PrognosticTKE"]["stochastic_entrainment"] = stoch_entr)
-calibrate_io ≠ nothing && (namelist["stats_io"]["calibrate_io"] = calibrate_io)
-skip_io      ≠ nothing && (namelist["stats_io"]["skip"] = skip_io)
+micro              ≠ nothing && (namelist["thermodynamics"]["quadrature_type"] = micro)
+entr               ≠ nothing && (namelist["turbulence"]["EDMF_PrognosticTKE"]["entrainment"] = entr)
+stoch_entr         ≠ nothing && (namelist["turbulence"]["EDMF_PrognosticTKE"]["stochastic_entrainment"] = stoch_entr)
+calibrate_io       ≠ nothing && (namelist["stats_io"]["calibrate_io"] = calibrate_io)
+skip_io            ≠ nothing && (namelist["stats_io"]["skip"] = skip_io)
+area_limiter_power ≠ nothing && (namelist["turbulence"]["EDMF_PrognosticTKE"]["area_limiter_power"] = area_limiter_power)
+entr_dim_scale     ≠ nothing && (namelist["turbulence"]["EDMF_PrognosticTKE"]["entr_dim_scale"] = entr_dim_scale)
 #! format: on
 
 ds_tc_filename, return_code = main(namelist)
