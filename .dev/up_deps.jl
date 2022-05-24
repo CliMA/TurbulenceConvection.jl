299168cbd180a5608b39275c36e6d4e48d7162f5
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-05-24 09:51:23 -0700

    Remove TC compat in perf, add info to up_deps

--- .dev/up_deps.jl
+++ .dev/up_deps.jl
@@ -9,6 +9,7 @@ dirs =
 
 cd(root) do
     for dir in dirs
+        @info "Pkg.up for environment $dir"
         cmd = `$(Base.julia_cmd()) --project=$dir -e 'import Pkg; Pkg.update()'`
         run(cmd)
     end
