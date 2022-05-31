caad10cf1a9963ae3bcdf8fcf3282d34d070c284
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-05-31 09:00:09 -0700

    Remove state from Simulation1D

--- perf/jet.jl
+++ perf/jet.jl
@@ -3,7 +3,7 @@ import JET
 
 case_name = "Bomex"
 sim = init_sim(case_name)
-prog = sim.state.prog
+prog = sim.prog
 tendencies = copy(prog)
-params = (; edmf = sim.edmf, param_set = sim.param_set, case = sim.case, TS = sim.TS, aux = sim.state.aux)
+params = (; edmf = sim.edmf, param_set = sim.param_set, case = sim.case, TS = sim.TS, aux = sim.aux)
 JET.@test_opt ∑tendencies!(tendencies, prog, params, sim.TS.t)
