41d5a37216ed6ddbe0cc76c1af7fb77a36b6f226
Anna Jaruga <ajaruga@caltech.edu>
2022-07-19 22:44:42 -0700

    trying to bring back the truncated gcm mesh

--- driver/common_spaces.jl
+++ driver/common_spaces.jl
@@ -30,10 +30,9 @@ function make_horizontal_space(mesh, quad)
     return space
 end
 
-function make_hybrid_spaces(h_space, z_max, z_elem, z_stretch)
+function make_hybrid_spaces(h_space, z_max, z_mesh)
     FT = eltype(z_max)
-    z_domain = CC.Domains.IntervalDomain(CCG.ZPoint(zero(z_max)), CCG.ZPoint(z_max); boundary_tags = (:bottom, :top))
-    z_mesh = CC.Meshes.IntervalMesh(z_domain, z_stretch; nelems = z_elem)
+
     @info "z heights" z_mesh.faces
     z_topology = CC.Topologies.IntervalTopology(z_mesh)
     z_space = CC.Spaces.CenterFiniteDifferenceSpace(z_topology)
@@ -73,51 +72,57 @@ function construct_mesh(namelist; FT = Float64)
     end
 
     z₀, z₁ = FT(0), FT(nz * Δz)
-    z_stretch = CC.Meshes.Uniform()
-    return (; z_stretch, z_max = z₁, z_elem = nz)
-end
 
+    if truncated_gcm_mesh
+        nzₛ = namelist["grid"]["stretch"]["nz"]
+        Δzₛ_surf = FT(namelist["grid"]["stretch"]["dz_surf"])
+        Δzₛ_top = FT(namelist["grid"]["stretch"]["dz_toa"])
+        zₛ_toa = FT(namelist["grid"]["stretch"]["z_toa"])
+
+        z_stretch = CC.Meshes.GeneralizedExponentialStretching(Δzₛ_surf, Δzₛ_top)
+        z_domain = CC.Domains.IntervalDomain(
+            CCG.ZPoint{FT}(z₀),
+            CCG.ZPoint{FT}(zₛ_toa),
+            boundary_tags = (:bottom, :top),
+        )
+        gcm_mesh = CC.Meshes.IntervalMesh(z_domain, z_stretch; nelems = nzₛ)
+        z_mesh = TC.TCMeshFromGCMMesh(gcm_mesh; z_max = z₁)
+    else
+        z_stretch = CC.Meshes.Uniform()
+        z_domain = CC.Domains.IntervalDomain(
+            CCG.ZPoint{FT}(z₀),
+            CCG.ZPoint{FT}(z₁),
+            boundary_tags = (:bottom, :top)
+        )
+        z_mesh = CC.Meshes.IntervalMesh(z_domain, z_stretch; nelems = z_elem)
+    end
+
+    return (; z_mesh = z_mesh, z_max = z₁)
+end
 
 function get_spaces(namelist, param_set, FT)
     center_space, face_space, svpc_space = if namelist["config"] == "sphere"
+
         h_elem = 1
         quad = CC.Spaces.Quadratures.GLL{2}()
         horizontal_mesh = cubed_sphere_mesh(; radius = FT(TCP.planet_radius(param_set)), h_elem)
         h_space = make_horizontal_space(horizontal_mesh, quad)
-        (; z_stretch, z_max, z_elem) = construct_mesh(namelist; FT = FT)
-        center_space, face_space, svpc_space = make_hybrid_spaces(h_space, z_max, z_elem, z_stretch)
-        center_space, face_space, svpc_space
+
+        (; z_mesh, z_max) = construct_mesh(namelist; FT = FT)
+
+        center_space, face_space, svpc_space = make_hybrid_spaces(h_space, z_max, z_mesh)
+        return center_space, face_space, svpc_space
+
     elseif namelist["config"] == "column" # single column (default)
+
         Δx = FT(1) # Note: This value shouldn't matter, since we only have 1 column.
         quad = CC.Spaces.Quadratures.GL{1}()
         horizontal_mesh = periodic_rectangle_mesh(; x_max = Δx, y_max = Δx, x_elem = 1, y_elem = 1)
         h_space = make_horizontal_space(horizontal_mesh, quad)
-        (; z_stretch, z_max, z_elem) = construct_mesh(namelist; FT = FT)
-        center_space, face_space, svpc_space = make_hybrid_spaces(h_space, z_max, z_elem, z_stretch)
-        center_space, face_space, svpc_space
+
+        (; z_mesh, z_max) = construct_mesh(namelist; FT = FT)
+
+        center_space, face_space, svpc_space = make_hybrid_spaces(h_space, z_max, z_mesh)
+        return center_space, face_space, svpc_space
     end
-    # if truncated_gcm_mesh
-    #     nzₛ = namelist["grid"]["stretch"]["nz"]
-    #     Δzₛ_surf = FT(namelist["grid"]["stretch"]["dz_surf"])
-    #     Δzₛ_top = FT(namelist["grid"]["stretch"]["dz_toa"])
-    #     zₛ_toa = FT(namelist["grid"]["stretch"]["z_toa"])
-    #     stretch = CC.Meshes.GeneralizedExponentialStretching(Δzₛ_surf, Δzₛ_top)
-    #     domain = CC.Domains.IntervalDomain(
-    #         CC.Geometry.ZPoint{FT}(z₀),
-    #         CC.Geometry.ZPoint{FT}(zₛ_toa),
-    #         boundary_tags = (:bottom, :top),
-    #     )
-    #     gcm_mesh = CC.Meshes.IntervalMesh(domain, stretch; nelems = nzₛ)
-    #     mesh = TC.TCMeshFromGCMMesh(gcm_mesh; z_max = z₁)
-    # else
-    #     CC.Meshes.Uniform()
-    #     domain = CC.Domains.IntervalDomain(
-    #         CC.Geometry.ZPoint{FT}(z₀),
-    #         CC.Geometry.ZPoint{FT}(z₁),
-    #         boundary_tags = (:bottom, :top),
-    #     )
-    #     mesh = CC.Meshes.IntervalMesh(domain, nelems = nz)
-    # end
-    # @info "z heights" mesh.faces
-    # return TC.Grid(mesh)
 end
