6bd9a494ab0d8d7126f0653a373ad6d52fdf4f4f
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-07-24 11:16:12 -0700

    Update flame script

--- perf/flame.jl
+++ perf/flame.jl
@@ -6,12 +6,16 @@ sim = init_sim(case_name)
 (prob, alg, kwargs) = solve_args(sim)
 integrator = ODE.init(prob, alg; kwargs...)
 
-ODE.step!(integrator) # force compilation
-Profile.clear_malloc_data()
-prof = Profile.@profile begin
-    for _ in 1:100
+function do_work!(integrator)
+    for _ in 1:1000
         ODE.step!(integrator)
     end
+end
+
+do_work!(integrator) # force compilation
+Profile.clear_malloc_data()
+prof = Profile.@profile begin
+    do_work!(integrator)
 end
 
 import PProf
