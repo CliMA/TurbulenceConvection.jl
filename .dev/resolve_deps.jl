c056b399342a8fea90c0c33e34ce5b4ed42d4b5b
Anna Jaruga <ajaruga@caltech.edu>
2022-09-12 13:55:26 -0700

    add resolve script

---
+++ .dev/resolve_deps.jl
@@ -0,0 +1,23 @@
+#=
+A simple script for updating the manifest
+files in all of our environments.
+=#
+
+root = dirname(@__DIR__)
+dirs =
+    (root, joinpath(root, "test"), joinpath(root, "perf"), joinpath(root, "docs"), joinpath(root, "integration_tests"))
+
+cd(root) do
+    for dir in dirs
+        @info "Pkg.resolve for environment $dir"
+        cmd = `$(Base.julia_cmd()) --project=$dir -e 'import Pkg; Pkg.resolve()'`
+        run(cmd)
+    end
+end
+
+# https://github.com/JuliaLang/Pkg.jl/issues/3014
+for dir in dirs
+    cd(dir) do
+        rm("LocalPreferences.toml"; force = true)
+    end
+end
