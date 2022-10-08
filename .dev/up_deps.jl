95315ee84403ae2019215d2af2f5c2c7f3514961
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-10-08 14:32:30 -0700

    Update up_deps, use JuliaFormatter 1

--- .dev/up_deps.jl
+++ .dev/up_deps.jl
@@ -4,13 +4,26 @@ files in all of our environments.
 =#
 
 root = dirname(@__DIR__)
-dirs =
-    (root, joinpath(root, "test"), joinpath(root, "perf"), joinpath(root, "docs"), joinpath(root, "integration_tests"))
+dirs = (
+    root,
+    joinpath(root, "test"),
+    joinpath(root, ".dev"),
+    joinpath(root, "perf"),
+    joinpath(root, "docs"),
+    joinpath(root, "integration_tests"),
+)
 
 cd(root) do
     for dir in dirs
-        @info "Pkg.up for environment $dir"
-        cmd = `$(Base.julia_cmd()) --project=$dir -e 'import Pkg; Pkg.update()'`
+        reldir = relpath(dir, root)
+        @info "Updating environment `$reldir`"
+        cmd = if dir == root
+            `$(Base.julia_cmd()) --project -e """import Pkg; Pkg.update()"""`
+        elseif dir == joinpath(root, ".dev")
+            `$(Base.julia_cmd()) --project=$reldir -e """import Pkg; Pkg.update()"""`
+        else
+            `$(Base.julia_cmd()) --project=$reldir -e """import Pkg; Pkg.develop(;path=\".\"); Pkg.update()"""`
+        end
         run(cmd)
     end
 end
